e1adf63108724f4a35517ba5a98dcf128d1048b7
iPedriNNz <iskol.gameplay@gmail.com>
2022-09-21 20:22:18 -0300

    slug automático e cadastro de produtos

--- produto/models.py
+++ produto/models.py
@@ -9,17 +9,18 @@ from django.db import models
 from PIL import Image
 import os
 from django.conf import settings
+from django.utils.text import slugify
 
 
 class Produto(models.Model):
-    nome = models.CharField(max_length=255)
+    nome = models.CharField(max_length=255, verbose_name='Produto')
     descricao_curta = models.TextField(max_length=255)
     descricao_longa = models.TextField()
     imagem = models.ImageField(
         upload_to='produto_imagens/%Y/%m/', blank=False, null=False)
-    slug = models.SlugField(unique=True)
-    preco_marketing = models.FloatField(default=0)
-    preco_marketing_promocional = models.FloatField(default=0)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+    preco_marketing = models.FloatField(default=0, verbose_name='Preço')
+    preco_marketing_promocional = models.FloatField(default=0, verbose_name='Preço Promo')
     tipo = models.CharField(
         default='V',
         max_length=1,
@@ -28,6 +29,17 @@ class Produto(models.Model):
             ('S', 'Simples')
         )
     )
+    
+    
+    # Formatação do preço formatado exibido na area admin
+    def get_preco_formatado(self):
+        return f'R$ {self.preco_marketing:.2f}'.replace('.', ',')
+    get_preco_formatado.short_description  = 'Preço'
+    
+    def get_preco_promo_formatado(self):
+        return f'R$ {self.preco_marketing_promocional:.2f}'.replace('.', ',')
+    get_preco_promo_formatado.short_description  = 'Preço Promocional'
+
 
     # Redimensionamento de imagem do produto
     @staticmethod
@@ -52,6 +64,11 @@ class Produto(models.Model):
         print('Imagem redimensionada com sucesso!')
 
     def save(self, *args, **kwargs):
+        
+        if not self.slug:
+            slug = slugify(self.nome)
+            self.slug = slug
+
         super().save(*args, **kwargs)
         max_image_size = 800
         if self.imagem:
@@ -75,3 +92,4 @@ class Variacao(models.Model):
     class Meta:
         verbose_name = 'Variação'
         verbose_name_plural = 'Variações'
+        
